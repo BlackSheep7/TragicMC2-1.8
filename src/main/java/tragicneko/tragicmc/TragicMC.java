6561e3407c87311f369f49d32d816b58e2abaede
TragicNeko <tragicfeint@gmail.com>
2016-03-27 12:34:45 -0500

    Bump version number (again)

--- src/main/java/tragicneko/tragicmc/TragicMC.java
+++ src/main/java/tragicneko/tragicmc/TragicMC.java
@@ -86,7 +86,7 @@ public class TragicMC
 {
 	public static final String MODNAME = "TragicMC 2";
 	public static final String MODID = "TragicMC";
-	public static final String VERSION = "4.47.3000";
+	public static final String VERSION = "4.47.3003";
 	public static final String ACCEPTED_VERSION = "[1.8.9]";
 	public static final String VERSION_JSON = "https://gist.githubusercontent.com/TragicNeko/d70456c2715e735920a1/raw/a7a25b91d3462b12e77c69e9166cd3b3fe7cdb1a/TragicMC2-Version.json";
 
