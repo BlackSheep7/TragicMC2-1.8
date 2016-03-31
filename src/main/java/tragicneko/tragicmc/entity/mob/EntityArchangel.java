7b73db05799f84a75c2fb9baf4760f89d4a57e70
TragicNeko <tragicfeint@gmail.com>
2016-03-31 01:55:25 -0500

    Updated/fixed Archangel movement

--- src/main/java/tragicneko/tragicmc/entity/mob/EntityArchangel.java
+++ src/main/java/tragicneko/tragicmc/entity/mob/EntityArchangel.java
@@ -1,14 +1,17 @@
 package tragicneko.tragicmc.entity.mob;
 
 import static tragicneko.tragicmc.TragicConfig.archangelStats;
+
 import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.EnumCreatureAttribute;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.EntityAIAttackOnCollide;
 import net.minecraft.entity.ai.EntityAIHurtByTarget;
 import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
+import net.minecraft.entity.ai.EntityMoveHelper;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.BlockPos;
@@ -18,10 +21,9 @@ import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-import tragicneko.tragicmc.TragicBlocks;
 import tragicneko.tragicmc.TragicConfig;
 import tragicneko.tragicmc.TragicEntities;
-import tragicneko.tragicmc.entity.alpha.EntityOverlordCore;
+import tragicneko.tragicmc.TragicMC;
 import tragicneko.tragicmc.util.DamageHelper;
 import tragicneko.tragicmc.util.WorldHelper;
 
@@ -30,16 +32,14 @@ public class EntityArchangel extends TragicMob {
 	public static final int DW_HOVER_TICKS = 20;
 	public static final int DW_TARGET_ID = 21;
 
-	public int courseChangeCooldown;
-	public double waypointX;
-	public double waypointY;
-	public double waypointZ;
-
 	public int hoverBuffer = 120;
+	public double wayDifference = 0;
+	public double prevWaypoint = 0;
 
 	public EntityArchangel(World par1World) {
 		super(par1World);
 		this.setSize(1.725F, 1.625F);
+		this.moveHelper = new EntityArchangel.FlyingMoveHelper(this);
 		this.isImmuneToFire = true;
 		this.experienceValue = 10;
 		this.tasks.addTask(0, new EntityAIAttackOnCollide(this, EntityLivingBase.class, 1.0D, true));
@@ -132,7 +132,6 @@ public class EntityArchangel extends TragicMob {
 
 		if (result)
 		{
-			this.courseChangeCooldown = -14;
 			if (this.getHoverTicks() > 0)
 			{
 				this.setHoverTicks(0);
@@ -166,7 +165,7 @@ public class EntityArchangel extends TragicMob {
 				double d1 = entity.posY - this.posY;
 				double d2 = entity.posZ - this.posZ;
 
-				for (int l = 0; l < 4; l++)
+				for (byte l = 0; l < 4; l++)
 				{
 					double d3 = 0.23D * l + (rand.nextDouble() * 0.25D);
 					this.worldObj.spawnParticle(EnumParticleTypes.CRIT, this.posX + d0 * d3, this.posY + d1 * d3 + 0.75D, this.posZ + d2 * d3, 0.0, 0.0, 0.0);
@@ -175,7 +174,7 @@ public class EntityArchangel extends TragicMob {
 
 				if (this.getHoverTicks() <= 120)
 				{
-					for (int i = 0; i < 4; i++)
+					for (byte i = 0; i < 4; i++)
 					{
 						this.worldObj.spawnParticle(EnumParticleTypes.FLAME, this.posX + rand.nextDouble() - rand.nextDouble(),
 								this.posY + 0.75 + rand.nextDouble() * 0.5, this.posZ + rand.nextDouble() - rand.nextDouble(), 0.0, 0.0, 0.0);
@@ -185,7 +184,7 @@ public class EntityArchangel extends TragicMob {
 
 			if (this.ticksExisted % 5 == 0 && rand.nextBoolean())
 			{
-				for (int l = 0; l < 2; ++l)
+				for (byte l = 0; l < 2; ++l)
 				{
 					this.worldObj.spawnParticle(EnumParticleTypes.CRIT,
 							this.posX + (this.rand.nextDouble() - rand.nextDouble()) * this.width * 1.5D,
@@ -204,63 +203,46 @@ public class EntityArchangel extends TragicMob {
 
 		if (this.getHoverTicks() <= 0)
 		{
-			double d0 = this.waypointX - this.posX;
-			double d1 = this.waypointY - this.posY;
-			double d2 = this.waypointZ - this.posZ;
+			double d0 = this.getMoveHelper().getX() - this.posX;
+			double d1 = this.getMoveHelper().getY() - this.posY;
+			double d2 = this.getMoveHelper().getZ() - this.posZ;
 			double d3 = d0 * d0 + d1 * d1 + d2 * d2;
 
-			if (d3 < 1.0D || d3 > 3600.0D)
+			if (!Double.isNaN(d3))
+			{
+				this.wayDifference = Math.abs(d3 - this.prevWaypoint);
+				this.prevWaypoint = d3;
+			}
+
+			if (d3 < 1.0D || d3 > 400.0D || this.wayDifference < 10)
 			{
 				if (this.getAttackTarget() != null)
 				{
-					this.waypointX = this.getAttackTarget().posX + (this.rand.nextFloat() * 2.0F - 1.0F) * 16.0F;
-					this.waypointY = this.getAttackTarget().posY + (this.rand.nextFloat() * 2.0F - 1.0F) * 16.0F;
-					this.waypointZ = this.getAttackTarget().posZ + (this.rand.nextFloat() * 2.0F - 1.0F) * 16.0F;
+					d0 = this.getAttackTarget().posX + (this.rand.nextFloat() * 2.0F - 1.0F) * 8.0F;
+					d1 = this.getAttackTarget().posY + (this.rand.nextFloat() * 2.0F - 1.0F) * 8.0F;
+					d2 = this.getAttackTarget().posZ + (this.rand.nextFloat() * 2.0F - 1.0F) * 8.0F;
 				}
 				else
 				{
-					this.waypointX = this.posX + (this.rand.nextFloat() * 2.0F - 1.0F) * 32.0F;
-					this.waypointY = this.posY + (this.rand.nextFloat() * 2.0F - 1.0F) * 32.0F;
-					this.waypointZ = this.posZ + (this.rand.nextFloat() * 2.0F - 1.0F) * 32.0F;
+					d0 = this.posX + (this.rand.nextFloat() * 2.0F - 1.0F) * 16.0F;
+					d1 = this.posY + (this.rand.nextFloat() * 2.0F - 1.0F) * 16.0F;
+					d2 = this.posZ + (this.rand.nextFloat() * 2.0F - 1.0F) * 16.0F;
 
-					if (this.waypointY - WorldHelper.getDistanceToGround(this) >= 20) this.waypointY -= 10;
+					int y = WorldHelper.getDistanceToGround(this);
+					if (d1 - y >= 20) d1 -= y / 2;
 				}
-			}
-
-			if (this.courseChangeCooldown-- <= 0)
-			{
-				this.courseChangeCooldown += this.rand.nextInt(5) + 2;
-				d3 = MathHelper.sqrt_double(d3);
-				double d4 = this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).getAttributeValue();
-
-				if (this.isCourseTraversable(this.waypointX, this.waypointY, this.waypointZ, d3))
-				{
-					this.motionX += d0 / d3 * d4;
-					this.motionY += d1 / d3 * d4;
-					this.motionZ += d2 / d3 * d4;
-				}
-				else
-				{
-					this.waypointX = this.posX;
-					this.waypointY = this.posY;
-					this.waypointZ = this.posZ;
-				}
+				
+				this.getMoveHelper().setMoveTo(d0, d1, d2, 1.0);
 			}
 
 			double d4 = this.getEntityAttribute(SharedMonsterAttributes.followRange).getAttributeValue();
 
-			if (this.getAttackTarget() != null && this.getAttackTarget().getDistanceSqToEntity(this) < d4 * d4 && this.getAttackTarget().getDistanceToEntity(this) > 6.0F && this.getHoverTicks() == 0)
+			if (this.getAttackTarget() != null && this.getAttackTarget().getDistanceSqToEntity(this) < d4 * d4 && this.getAttackTarget().getDistanceToEntity(this) > 6.0F && this.getHoverTicks() == 0 && rand.nextInt(16) == 0)
 			{
-				double d5 = this.getAttackTarget().posX - this.posX;
-				double d7 = this.getAttackTarget().posZ - this.posZ;
-				this.renderYawOffset = this.rotationYaw = -((float)Math.atan2(d5, d7)) * 180.0F / (float)Math.PI;
-
 				if (this.canEntityBeSeen(this.getAttackTarget()) && this.hoverBuffer == 0) this.setHoverTicks(250);
 			}
-			else
-			{
-				this.renderYawOffset = this.rotationYaw = -((float)Math.atan2(this.motionX, this.motionZ)) * 180.0F / (float)Math.PI;
-			}
+			
+			if (WorldHelper.getDistanceToGround(this) <= 1.5) this.motionY = 0.1D;
 		}
 		else
 		{
@@ -279,8 +261,6 @@ public class EntityArchangel extends TragicMob {
 			}
 
 			if (this.getAttackTarget() == null || this.getAttackTarget().isDead || !this.canEntityBeSeen(this.getAttackTarget())) this.setHoverTicks(0);
-
-
 		}
 
 		if (this.getAttackTarget() != null && this.getAttackTarget().isDead) this.setAttackTarget(null);
@@ -293,26 +273,17 @@ public class EntityArchangel extends TragicMob {
 		{
 			this.setTargetId(0);
 		}
-	}
-
-	private boolean isCourseTraversable(double p_70790_1_, double p_70790_3_, double p_70790_5_, double p_70790_7_)
-	{
-		double d4 = (this.waypointX - this.posX) / p_70790_7_;
-		double d5 = (this.waypointY - this.posY) / p_70790_7_;
-		double d6 = (this.waypointZ - this.posZ) / p_70790_7_;
-		AxisAlignedBB axisalignedbb = this.getEntityBoundingBox();
-
-		for (int i = 1; i < p_70790_7_; ++i)
+		
+		if (this.getAttackTarget() != null)
 		{
-			axisalignedbb.offset(d4, d5, d6);
-
-			if (!this.worldObj.getCollidingBoundingBoxes(this, axisalignedbb).isEmpty())
-			{
-				return false;
-			}
+			double d5 = this.getAttackTarget().posX - this.posX;
+			double d7 = this.getAttackTarget().posZ - this.posZ;
+			this.renderYawOffset = this.rotationYaw = -((float)Math.atan2(d5, d7)) * 180.0F / (float)Math.PI;
 		}
-
-		return true;
+		else
+		{
+			this.renderYawOffset = this.rotationYaw = -((float)Math.atan2(this.motionX, this.motionZ)) * 180.0F / (float)Math.PI;
+		}
 	}
 
 	@Override
@@ -331,63 +302,63 @@ public class EntityArchangel extends TragicMob {
 
 	@Override
 	public void updateFallState(double par1, boolean par2, Block block, BlockPos pos) {}
-
+	
 	@Override
 	public void moveEntityWithHeading(float strafe, float forward)
-	{
-		if (this.isInWater())
-		{
-			this.moveFlying(strafe, forward, 0.02F);
-			this.moveEntity(this.motionX, this.motionY, this.motionZ);
-			this.motionX *= 0.800000011920929D;
-			this.motionY *= 0.800000011920929D;
-			this.motionZ *= 0.800000011920929D;
-		}
-		else if (this.isInLava())
-		{
-			this.moveFlying(strafe, forward, 0.02F);
-			this.moveEntity(this.motionX, this.motionY, this.motionZ);
-			this.motionX *= 0.5D;
-			this.motionY *= 0.5D;
-			this.motionZ *= 0.5D;
-		}
-		else
-		{
-			float f2 = 0.91F;
+    {
+        if (this.isInWater())
+        {
+            this.moveFlying(strafe, forward, 0.02F);
+            this.moveEntity(this.motionX, this.motionY, this.motionZ);
+            this.motionX *= 0.800000011920929D;
+            this.motionY *= 0.800000011920929D;
+            this.motionZ *= 0.800000011920929D;
+        }
+        else if (this.isInLava())
+        {
+            this.moveFlying(strafe, forward, 0.02F);
+            this.moveEntity(this.motionX, this.motionY, this.motionZ);
+            this.motionX *= 0.5D;
+            this.motionY *= 0.5D;
+            this.motionZ *= 0.5D;
+        }
+        else
+        {
+            float f = 0.91F;
 
-			if (this.onGround)
-			{
-				f2 = this.worldObj.getBlockState(new BlockPos(MathHelper.floor_double(this.posX), MathHelper.floor_double(this.getEntityBoundingBox().minY) - 1, MathHelper.floor_double(this.posZ))).getBlock().slipperiness * 0.91F;
-			}
+            if (this.onGround)
+            {
+                f = this.worldObj.getBlockState(new BlockPos(MathHelper.floor_double(this.posX), MathHelper.floor_double(this.getEntityBoundingBox().minY) - 1, MathHelper.floor_double(this.posZ))).getBlock().slipperiness * 0.91F;
+            }
 
-			float f3 = 0.16277136F / (f2 * f2 * f2);
-			this.moveFlying(strafe, forward, this.onGround ? 0.1F * f3 : 0.02F);
-			f2 = 0.91F;
+            float f1 = 0.16277136F / (f * f * f);
+            this.moveFlying(strafe, forward, this.onGround ? 0.1F * f1 : 0.02F);
+            f = 0.91F;
 
-			if (this.onGround)
-			{
-				f2 = this.worldObj.getBlockState(new BlockPos(MathHelper.floor_double(this.posX), MathHelper.floor_double(this.getEntityBoundingBox().minY) - 1, MathHelper.floor_double(this.posZ))).getBlock().slipperiness * 0.91F;
-			}
+            if (this.onGround)
+            {
+                f = this.worldObj.getBlockState(new BlockPos(MathHelper.floor_double(this.posX), MathHelper.floor_double(this.getEntityBoundingBox().minY) - 1, MathHelper.floor_double(this.posZ))).getBlock().slipperiness * 0.91F;
+            }
 
-			this.moveEntity(this.motionX, this.motionY, this.motionZ);
-			this.motionX *= f2;
-			this.motionY *= f2;
-			this.motionZ *= f2;
-		}
+            this.moveEntity(this.motionX, this.motionY, this.motionZ);
+            this.motionX *= (double)f;
+            this.motionY *= (double)f;
+            this.motionZ *= (double)f;
+        }
 
-		this.prevLimbSwingAmount = this.limbSwingAmount;
-		double d1 = this.posX - this.prevPosX;
-		double d0 = this.posZ - this.prevPosZ;
-		float f4 = MathHelper.sqrt_double(d1 * d1 + d0 * d0) * 4.0F;
+        this.prevLimbSwingAmount = this.limbSwingAmount;
+        double d1 = this.posX - this.prevPosX;
+        double d0 = this.posZ - this.prevPosZ;
+        float f2 = MathHelper.sqrt_double(d1 * d1 + d0 * d0) * 4.0F;
 
-		if (f4 > 1.0F)
-		{
-			f4 = 1.0F;
-		}
+        if (f2 > 1.0F)
+        {
+            f2 = 1.0F;
+        }
 
-		this.limbSwingAmount += (f4 - this.limbSwingAmount) * 0.4F;
-		this.limbSwing += this.limbSwingAmount;
-	}
+        this.limbSwingAmount += (f2 - this.limbSwingAmount) * 0.4F;
+        this.limbSwing += this.limbSwingAmount;
+    }
 
 	@Override
 	public boolean isOnLadder()
@@ -398,24 +369,16 @@ public class EntityArchangel extends TragicMob {
 	@Override
 	public void readEntityFromNBT(NBTTagCompound tag) {
 		super.readEntityFromNBT(tag);
-		if (tag.hasKey("courseChangeCooldown")) this.courseChangeCooldown = tag.getInteger("courseChangeCooldown");
 		if (tag.hasKey("hoverBuffer")) this.hoverBuffer = tag.getInteger("hoverBuffer");
 		if (tag.hasKey("hoverTicks")) this.setHoverTicks(tag.getInteger("hoverTicks"));
-		if (tag.hasKey("waypointX")) this.waypointX = tag.getDouble("waypointX");
-		if (tag.hasKey("waypointY")) this.waypointY = tag.getDouble("waypointY");
-		if (tag.hasKey("waypointZ")) this.waypointZ = tag.getDouble("waypointZ");
 	}
 
 	@Override
 	public void writeEntityToNBT(NBTTagCompound tag)
 	{
 		super.writeEntityToNBT(tag);
-		tag.setInteger("courseChangeCooldown", this.courseChangeCooldown);
 		tag.setInteger("hoverBuffer", this.hoverBuffer);
 		tag.setInteger("hoverTicks", this.getHoverTicks());
-		tag.setDouble("waypointX", this.waypointX);
-		tag.setDouble("waypointY", this.waypointY);
-		tag.setDouble("waypointZ", this.waypointZ);
 	}
 
 	@Override
@@ -458,4 +421,63 @@ public class EntityArchangel extends TragicMob {
 	public boolean getIllumination() {
 		return true;
 	}
+	
+	public static class FlyingMoveHelper extends EntityMoveHelper
+    {
+        private EntityLiving parentEntity;
+        private int courseChangeCooldown;
+
+        public FlyingMoveHelper(EntityLiving parent)
+        {
+            super(parent);
+            this.parentEntity = parent;
+        }
+
+        public void onUpdateMoveHelper()
+        {
+            if (this.update)
+            {
+                if (this.courseChangeCooldown-- <= 0)
+                {
+                	double d0 = this.posX - this.parentEntity.posX;
+                    double d1 = this.posY - this.parentEntity.posY;
+                    double d2 = this.posZ - this.parentEntity.posZ;
+                    double d3 = d0 * d0 + d1 * d1 + d2 * d2;
+                    this.courseChangeCooldown += this.parentEntity.getRNG().nextInt(5) + 2;
+                    d3 = (double)MathHelper.sqrt_double(d3);
+
+                    if (this.isNotColliding(this.posX, this.posY, this.posZ, d3))
+                    {
+                        this.parentEntity.motionX += d0 / d3 * 0.1D;
+                        this.parentEntity.motionY += d1 / d3 * 0.1D;
+                        this.parentEntity.motionZ += d2 / d3 * 0.1D;
+                    }
+                    else
+                    {
+                        this.update = false;
+                    }
+                }
+            }
+        }
+        
+        private boolean isNotColliding(double wayX, double wayY, double wayZ, double dist)
+        {
+            double d0 = (wayX - this.parentEntity.posX) / dist;
+            double d1 = (wayY - this.parentEntity.posY) / dist;
+            double d2 = (wayZ - this.parentEntity.posZ) / dist;
+            AxisAlignedBB axisalignedbb = this.parentEntity.getEntityBoundingBox();
+
+            for (int i = 1; (double)i < dist; ++i)
+            {
+                axisalignedbb = axisalignedbb.offset(d0, d1, d2);
+
+                if (!this.parentEntity.worldObj.getCollidingBoundingBoxes(this.parentEntity, axisalignedbb).isEmpty())
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
 }
