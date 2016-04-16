78fe378552245c23ffbdccb894e8550b5f2ab1ff
TragicNeko <tragicfeint@gmail.com>
2016-04-16 04:33:33 -0500

    Append mod id on ResourceLocations in Enchantments and Potions

--- src/main/java/tragicneko/tragicmc/TragicEnchantments.java
+++ src/main/java/tragicneko/tragicmc/TragicEnchantments.java
@@ -22,28 +22,28 @@ public class TragicEnchantments {
 
 	public static void load()
 	{
-		if (TragicConfig.getBoolean("allowDecay")) Decay = (new EnchantmentDamageBoost(TragicConfig.getInt("decayID"), new ResourceLocation("decay"), TragicConfig.getInt("decayWeight"), 0));
-		if (TragicConfig.getBoolean("allowAbsolve")) Absolve = (new EnchantmentDamageBoost(TragicConfig.getInt("absolveID"), new ResourceLocation("absolve"), TragicConfig.getInt("absolveWeight"), 1));
-		if (TragicConfig.getBoolean("allowSlay")) Slay = (new EnchantmentDamageBoost(TragicConfig.getInt("slayID"), new ResourceLocation("slay"), TragicConfig.getInt("slayWeight"), 2));
-		if (TragicConfig.getBoolean("allowVampirism")) Vampirism = (new EnchantmentWeaponExtra(TragicConfig.getInt("vampirismID"), new ResourceLocation("vampirism"), TragicConfig.getInt("vampirismWeight"), 0));
-		if (TragicConfig.getBoolean("allowLeech")) Leech = (new EnchantmentWeaponExtra(TragicConfig.getInt("leechID"), new ResourceLocation("leech"), TragicConfig.getInt("leechWeight"), 1));
-		if (TragicConfig.getBoolean("allowConsume")) Consume = (new EnchantmentWeaponExtra(TragicConfig.getInt("consumeID"), new ResourceLocation("consume"), TragicConfig.getInt("consumeWeight"), 2));
-		if (TragicConfig.getBoolean("allowDistract")) Distract = (new EnchantmentWeaponExtra(TragicConfig.getInt("distractID"), new ResourceLocation("distract"), TragicConfig.getInt("distractWeight"), 3));
-		if (TragicConfig.getBoolean("allowCombustion")) Combustion = (new EnchantmentCombustion(TragicConfig.getInt("combustionID"), new ResourceLocation("combustion"), TragicConfig.getInt("combustionWeight"), EnumEnchantmentType.DIGGER));
-		if (TragicConfig.getBoolean("allowMultiply")) Multiply = (new EnchantmentMultiply(TragicConfig.getInt("multiplyID"), new ResourceLocation("multiply"), TragicConfig.getInt("multiplyWeight"), EnumEnchantmentType.BOW));
-		if (TragicConfig.getBoolean("allowRuneBreak")) RuneBreak = (new EnchantmentDamageBoost(TragicConfig.getInt("runeBreakID"), new ResourceLocation("runeBreak"), TragicConfig.getInt("runeBreakWeight"), 3));
-		if (TragicConfig.getBoolean("allowReach")) Reach = (new EnchantmentRange(TragicConfig.getInt("reachID"), new ResourceLocation("reach"), TragicConfig.getInt("reachWeight"), EnumEnchantmentType.WEAPON));
-		if (TragicConfig.getBoolean("allowUnbreakable")) Unbreakable = (new EnchantmentUnbreakable(TragicConfig.getInt("unbreakableID"), new ResourceLocation("unbreakable"), TragicConfig.getInt("unbreakableWeight"), EnumEnchantmentType.BREAKABLE));
-		if (TragicConfig.getBoolean("allowRust")) Rust = (new EnchantmentWeaponExtra(TragicConfig.getInt("rustID"), new ResourceLocation("rust"), TragicConfig.getInt("rustWeight"), 4));
-		if (TragicConfig.getBoolean("allowVeteran")) Veteran = (new EnchantmentVeteran(TragicConfig.getInt("veteranID"), new ResourceLocation("veteran"), TragicConfig.getInt("veteranWeight"), EnumEnchantmentType.DIGGER));
+		if (TragicConfig.getBoolean("allowDecay")) Decay = (new EnchantmentDamageBoost(TragicConfig.getInt("decayID"), new ResourceLocation("tragicmc:decay"), TragicConfig.getInt("decayWeight"), 0));
+		if (TragicConfig.getBoolean("allowAbsolve")) Absolve = (new EnchantmentDamageBoost(TragicConfig.getInt("absolveID"), new ResourceLocation("tragicmc:absolve"), TragicConfig.getInt("absolveWeight"), 1));
+		if (TragicConfig.getBoolean("allowSlay")) Slay = (new EnchantmentDamageBoost(TragicConfig.getInt("slayID"), new ResourceLocation("tragicmc:slay"), TragicConfig.getInt("slayWeight"), 2));
+		if (TragicConfig.getBoolean("allowVampirism")) Vampirism = (new EnchantmentWeaponExtra(TragicConfig.getInt("vampirismID"), new ResourceLocation("tragicmc:vampirism"), TragicConfig.getInt("vampirismWeight"), 0));
+		if (TragicConfig.getBoolean("allowLeech")) Leech = (new EnchantmentWeaponExtra(TragicConfig.getInt("leechID"), new ResourceLocation("tragicmc:leech"), TragicConfig.getInt("leechWeight"), 1));
+		if (TragicConfig.getBoolean("allowConsume")) Consume = (new EnchantmentWeaponExtra(TragicConfig.getInt("consumeID"), new ResourceLocation("tragicmc:consume"), TragicConfig.getInt("consumeWeight"), 2));
+		if (TragicConfig.getBoolean("allowDistract")) Distract = (new EnchantmentWeaponExtra(TragicConfig.getInt("distractID"), new ResourceLocation("tragicmc:distract"), TragicConfig.getInt("distractWeight"), 3));
+		if (TragicConfig.getBoolean("allowCombustion")) Combustion = (new EnchantmentCombustion(TragicConfig.getInt("combustionID"), new ResourceLocation("tragicmc:combustion"), TragicConfig.getInt("combustionWeight"), EnumEnchantmentType.DIGGER));
+		if (TragicConfig.getBoolean("allowMultiply")) Multiply = (new EnchantmentMultiply(TragicConfig.getInt("multiplyID"), new ResourceLocation("tragicmc:multiply"), TragicConfig.getInt("multiplyWeight"), EnumEnchantmentType.BOW));
+		if (TragicConfig.getBoolean("allowRuneBreak")) RuneBreak = (new EnchantmentDamageBoost(TragicConfig.getInt("runeBreakID"), new ResourceLocation("tragicmc:runeBreak"), TragicConfig.getInt("runeBreakWeight"), 3));
+		if (TragicConfig.getBoolean("allowReach")) Reach = (new EnchantmentRange(TragicConfig.getInt("reachID"), new ResourceLocation("tragicmc:reach"), TragicConfig.getInt("reachWeight"), EnumEnchantmentType.WEAPON));
+		if (TragicConfig.getBoolean("allowUnbreakable")) Unbreakable = (new EnchantmentUnbreakable(TragicConfig.getInt("unbreakableID"), new ResourceLocation("tragicmc:unbreakable"), TragicConfig.getInt("unbreakableWeight"), EnumEnchantmentType.BREAKABLE));
+		if (TragicConfig.getBoolean("allowRust")) Rust = (new EnchantmentWeaponExtra(TragicConfig.getInt("rustID"), new ResourceLocation("tragicmc:rust"), TragicConfig.getInt("rustWeight"), 4));
+		if (TragicConfig.getBoolean("allowVeteran")) Veteran = (new EnchantmentVeteran(TragicConfig.getInt("veteranID"), new ResourceLocation("tragicmc:veteran"), TragicConfig.getInt("veteranWeight"), EnumEnchantmentType.DIGGER));
 
-		if (TragicConfig.getBoolean("allowIgnition"))Ignition = (new EnchantmentArmorExtra(TragicConfig.getInt("ignitionID"), new ResourceLocation("ignition"), TragicConfig.getInt("ignitionWeight"), 0));
-		if (TragicConfig.getBoolean("allowParalysis"))Paralysis = (new EnchantmentArmorExtra(TragicConfig.getInt("paralysisID"), new ResourceLocation("paralysis"), TragicConfig.getInt("paralysisWeight"), 1));
-		if (TragicConfig.getBoolean("allowToxicity"))Toxicity = (new EnchantmentArmorExtra(TragicConfig.getInt("toxicityID"), new ResourceLocation("toxicity"), TragicConfig.getInt("toxicityWeight"), 2));
-		if (TragicConfig.getBoolean("allowElasticity"))Elasticity = (new EnchantmentArmorExtra(TragicConfig.getInt("elasticityID"), new ResourceLocation("elasticity"), TragicConfig.getInt("elasticityWeight"), 3));
-		if (TragicConfig.getBoolean("allowDeathTouch"))DeathTouch = (new EnchantmentArmorExtra(TragicConfig.getInt("deathTouchID"), new ResourceLocation("deathTouch"), TragicConfig.getInt("deathTouchWeight"), 4));
-		if (TragicConfig.getBoolean("allowAgility")) Agility = (new EnchantmentAgility(TragicConfig.getInt("agilityID"), new ResourceLocation("agility"), TragicConfig.getInt("agilityWeight"), EnumEnchantmentType.ARMOR));
-		if (TragicConfig.getBoolean("allowRuneWalker")) RuneWalker = (new EnchantmentRuneWalker(TragicConfig.getInt("runeWalkerID"), new ResourceLocation("runeWalker"), TragicConfig.getInt("runeWalkerID"), EnumEnchantmentType.ARMOR));
-		if (TragicConfig.getBoolean("allowLuminescence")) Luminescence = (new EnchantmentLuminescence(TragicConfig.getInt("luminescenceID"), new ResourceLocation("luminescence"), TragicConfig.getInt("luminescenceID"), EnumEnchantmentType.ALL));
+		if (TragicConfig.getBoolean("allowIgnition"))Ignition = (new EnchantmentArmorExtra(TragicConfig.getInt("ignitionID"), new ResourceLocation("tragicmc:ignition"), TragicConfig.getInt("ignitionWeight"), 0));
+		if (TragicConfig.getBoolean("allowParalysis"))Paralysis = (new EnchantmentArmorExtra(TragicConfig.getInt("paralysisID"), new ResourceLocation("tragicmc:paralysis"), TragicConfig.getInt("paralysisWeight"), 1));
+		if (TragicConfig.getBoolean("allowToxicity"))Toxicity = (new EnchantmentArmorExtra(TragicConfig.getInt("toxicityID"), new ResourceLocation("tragicmc:toxicity"), TragicConfig.getInt("toxicityWeight"), 2));
+		if (TragicConfig.getBoolean("allowElasticity"))Elasticity = (new EnchantmentArmorExtra(TragicConfig.getInt("elasticityID"), new ResourceLocation("tragicmc:elasticity"), TragicConfig.getInt("elasticityWeight"), 3));
+		if (TragicConfig.getBoolean("allowDeathTouch"))DeathTouch = (new EnchantmentArmorExtra(TragicConfig.getInt("deathTouchID"), new ResourceLocation("tragicmc:deathTouch"), TragicConfig.getInt("deathTouchWeight"), 4));
+		if (TragicConfig.getBoolean("allowAgility")) Agility = (new EnchantmentAgility(TragicConfig.getInt("agilityID"), new ResourceLocation("tragicmc:agility"), TragicConfig.getInt("agilityWeight"), EnumEnchantmentType.ARMOR));
+		if (TragicConfig.getBoolean("allowRuneWalker")) RuneWalker = (new EnchantmentRuneWalker(TragicConfig.getInt("runeWalkerID"), new ResourceLocation("tragicmc:runeWalker"), TragicConfig.getInt("runeWalkerID"), EnumEnchantmentType.ARMOR));
+		if (TragicConfig.getBoolean("allowLuminescence")) Luminescence = (new EnchantmentLuminescence(TragicConfig.getInt("luminescenceID"), new ResourceLocation("tragicmc:luminescence"), TragicConfig.getInt("luminescenceID"), EnumEnchantmentType.ALL));
 	}
 }
--- src/main/java/tragicneko/tragicmc/TragicPotion.java
+++ src/main/java/tragicneko/tragicmc/TragicPotion.java
@@ -40,17 +40,17 @@ public class TragicPotion extends Potion {
 
 	public static void load()
 	{
-		if (TragicConfig.getBoolean("allowCorruption")) Corruption = (new TragicPotion(new ResourceLocation("corruption"), true, 0x000000).setPotionName("potion.corruption"));
-		if (TragicConfig.getBoolean("allowDisorientation")) Disorientation = (new TragicPotion(new ResourceLocation("disorientation"), true, 0x521A55).setPotionName("potion.disorientation"));
-		if (TragicConfig.getBoolean("allowStun")) Stun = (new TragicPotion(new ResourceLocation("stun"), true, 0xC2DC7D).setPotionName("potion.stun").registerPotionAttributeModifier(SharedMonsterAttributes.movementSpeed, "d35fe2b0-2aca-4d5e-b3e3-3fe041dbaf15", -3.0D, 2));
-		if (TragicConfig.getBoolean("allowFear")) Fear = (new TragicPotion(new ResourceLocation("fear"), true, 0x4A4A5F).setPotionName("potion.fear"));
-		if (TragicConfig.getBoolean("allowCripple")) Cripple = (new TragicPotion(new ResourceLocation("cripple"), true, 0x640000).setPotionName("potion.cripple").registerPotionAttributeModifier(SharedMonsterAttributes.maxHealth, "04df3109-9bca-4517-8855-8064742028e4", -2.0D, 0));
-		if (TragicConfig.getBoolean("allowMalnourish")) Malnourish = (new TragicPotion(new ResourceLocation("malnourish"), true, 0x59701C).setPotionName("potion.malnourish"));
-		if (TragicConfig.getBoolean("allowSubmission")) Submission = (new TragicPotion(new ResourceLocation("submission"), true, 0xB152B7).setPotionName("potion.submission"));
-		if (TragicConfig.getBoolean("allowInhibit")) Inhibit = (new TragicPotion(new ResourceLocation("inhibit"), true, 0x232323).setPotionName("potion.inhibit"));
-		if (TragicConfig.getBoolean("allowLeadFoot")) LeadFoot = (new TragicPotion(new ResourceLocation("leadFoot"), true, 0x78AB76).setPotionName("potion.leadFoot"));
-		if (TragicConfig.getBoolean("allowHacked")) Hacked = (new TragicPotion(new ResourceLocation("hacked"), true, 0xC6F6FF).setPotionName("potion.hacked"));
-		if (TragicConfig.getBoolean("allowBurned")) Burned = (new TragicPotion(new ResourceLocation("burned"), true, 0xFF0000).setPotionName("potion.burned"));
+		if (TragicConfig.getBoolean("allowCorruption")) Corruption = (new TragicPotion(new ResourceLocation("tragicmc:corruption"), true, 0x000000).setPotionName("potion.corruption"));
+		if (TragicConfig.getBoolean("allowDisorientation")) Disorientation = (new TragicPotion(new ResourceLocation("tragicmc:disorientation"), true, 0x521A55).setPotionName("potion.disorientation"));
+		if (TragicConfig.getBoolean("allowStun")) Stun = (new TragicPotion(new ResourceLocation("tragicmc:stun"), true, 0xC2DC7D).setPotionName("potion.stun").registerPotionAttributeModifier(SharedMonsterAttributes.movementSpeed, "d35fe2b0-2aca-4d5e-b3e3-3fe041dbaf15", -3.0D, 2));
+		if (TragicConfig.getBoolean("allowFear")) Fear = (new TragicPotion(new ResourceLocation("tragicmc:fear"), true, 0x4A4A5F).setPotionName("potion.fear"));
+		if (TragicConfig.getBoolean("allowCripple")) Cripple = (new TragicPotion(new ResourceLocation("tragicmc:cripple"), true, 0x640000).setPotionName("potion.cripple").registerPotionAttributeModifier(SharedMonsterAttributes.maxHealth, "04df3109-9bca-4517-8855-8064742028e4", -2.0D, 0));
+		if (TragicConfig.getBoolean("allowMalnourish")) Malnourish = (new TragicPotion(new ResourceLocation("tragicmc:malnourish"), true, 0x59701C).setPotionName("potion.malnourish"));
+		if (TragicConfig.getBoolean("allowSubmission")) Submission = (new TragicPotion(new ResourceLocation("tragicmc:submission"), true, 0xB152B7).setPotionName("potion.submission"));
+		if (TragicConfig.getBoolean("allowInhibit")) Inhibit = (new TragicPotion(new ResourceLocation("tragicmc:inhibit"), true, 0x232323).setPotionName("potion.inhibit"));
+		if (TragicConfig.getBoolean("allowLeadFoot")) LeadFoot = (new TragicPotion(new ResourceLocation("tragicmc:leadFoot"), true, 0x78AB76).setPotionName("potion.leadFoot"));
+		if (TragicConfig.getBoolean("allowHacked")) Hacked = (new TragicPotion(new ResourceLocation("tragicmc:hacked"), true, 0xC6F6FF).setPotionName("potion.hacked"));
+		if (TragicConfig.getBoolean("allowBurned")) Burned = (new TragicPotion(new ResourceLocation("tragicmc:burned"), true, 0xFF0000).setPotionName("potion.burned"));
 		//Deafening = new TragicPotion(TragicConfig.idHacked + 1, true, 0x000000).setPotionName("potion.deafening");
 		//Nightmare = new TragicPotion(TragicConfig.idHacked + 2, true, 0x000000).setPotionName("potion.nightmare");
 		//Exasperate = new TragicPotion(TragicConfig.idHacked + 3, true, 0x000000).setPotionName("potion.exasperate");
@@ -75,15 +75,15 @@ public class TragicPotion extends Potion {
 		}
 		}.setPotionName("potion.bleed"); */
 
-		if (TragicConfig.getBoolean("allowFlight")) Flight = (new TragicPotion(new ResourceLocation("flight"), false, 0xFDDC69).setPotionName("potion.flight"));
-		if (TragicConfig.getBoolean("allowAquaSuperiority")) AquaSuperiority = (new TragicPotion(new ResourceLocation("aquaSuperiority"), true, 0x69B9FD).setPotionName("potion.aquaSuperiority"));
-		if (TragicConfig.getBoolean("allowImmunity")) Immunity = (new TragicPotion(new ResourceLocation("immunity"), false, 0xBABABA).setPotionName("potion.immunity"));
-		if (TragicConfig.getBoolean("allowResurrection")) Resurrection = (new TragicPotion(new ResourceLocation("resurrection"), false, 0x9DFD69).setPotionName("potion.resurrection"));
-		if (TragicConfig.getBoolean("allowHarmony")) Harmony = (new TragicPotion(new ResourceLocation("harmony"), false, 0xE67CED).setPotionName("potion.harmony").registerPotionAttributeModifier(SharedMonsterAttributes.followRange, "ac2b9b0e-2eb2-443f-8ca2-a045f36f9e73", -256.0, 2));
-		if (TragicConfig.getBoolean("allowInvulnerability")) Invulnerability = (new TragicPotion(new ResourceLocation("invulnerability"), false, 0xD5ECFF).setPotionName("potion.invulnerability").registerPotionAttributeModifier(SharedMonsterAttributes.movementSpeed, "43d74ab6-d058-40e4-9761-70b0b80b8743", -0.2D, 2));
-		if (TragicConfig.getBoolean("allowClarity")) Clarity = (new TragicPotion(new ResourceLocation("clarity"), false, 0xFFFFFF).setPotionName("potion.clarity"));
-		if (TragicConfig.getBoolean("allowConvergence")) Convergence = (new TragicPotion(new ResourceLocation("convergence"), false, 0x9B2525).setPotionName("potion.convergence"));
-		if (TragicConfig.getBoolean("allowDivinity")) Divinity = (new TragicPotion(new ResourceLocation("divinity"), false, 0xFFFFFF).setPotionName("potion.divinity"));
+		if (TragicConfig.getBoolean("allowFlight")) Flight = (new TragicPotion(new ResourceLocation("tragicmc:flight"), false, 0xFDDC69).setPotionName("potion.flight"));
+		if (TragicConfig.getBoolean("allowAquaSuperiority")) AquaSuperiority = (new TragicPotion(new ResourceLocation("tragicmc:aquaSuperiority"), true, 0x69B9FD).setPotionName("potion.aquaSuperiority"));
+		if (TragicConfig.getBoolean("allowImmunity")) Immunity = (new TragicPotion(new ResourceLocation("tragicmc:immunity"), false, 0xBABABA).setPotionName("potion.immunity"));
+		if (TragicConfig.getBoolean("allowResurrection")) Resurrection = (new TragicPotion(new ResourceLocation("tragicmc:resurrection"), false, 0x9DFD69).setPotionName("potion.resurrection"));
+		if (TragicConfig.getBoolean("allowHarmony")) Harmony = (new TragicPotion(new ResourceLocation("tragicmc:harmony"), false, 0xE67CED).setPotionName("potion.harmony").registerPotionAttributeModifier(SharedMonsterAttributes.followRange, "ac2b9b0e-2eb2-443f-8ca2-a045f36f9e73", -256.0, 2));
+		if (TragicConfig.getBoolean("allowInvulnerability")) Invulnerability = (new TragicPotion(new ResourceLocation("tragicmc:invulnerability"), false, 0xD5ECFF).setPotionName("potion.invulnerability").registerPotionAttributeModifier(SharedMonsterAttributes.movementSpeed, "43d74ab6-d058-40e4-9761-70b0b80b8743", -0.2D, 2));
+		if (TragicConfig.getBoolean("allowClarity")) Clarity = (new TragicPotion(new ResourceLocation("tragicmc:clarity"), false, 0xFFFFFF).setPotionName("potion.clarity"));
+		if (TragicConfig.getBoolean("allowConvergence")) Convergence = (new TragicPotion(new ResourceLocation("tragicmc:convergence"), false, 0x9B2525).setPotionName("potion.convergence"));
+		if (TragicConfig.getBoolean("allowDivinity")) Divinity = (new TragicPotion(new ResourceLocation("tragicmc:divinity"), false, 0xFFFFFF).setPotionName("potion.divinity"));
 	}
 
 	public static void setPotionIcons()
